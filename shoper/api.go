b3c70dba7b44e63c426c529f538f5309337e332e
h1x <h1x@linuxconsole.net>
2022-09-24 23:54:58 +0200

    create a general function for calling api

--- shoper/api.go
+++ shoper/api.go
@@ -1,13 +1,70 @@
 package shoper
 
 import (
-	//"encoding/json"
-	//"errors"
+	"encoding/json"
+	"errors"
 	"fmt"
-	//"net/http"
+	"net/http"
+	"strconv"
 )
 
-func (s *Session) callApi(method, apiString, data string) error {
-	fmt.Println()
+func (s *Session) callApi(url, method, data string, res interface{}) error {
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("User-Agent", "curl/7.77.0")
+	req.Header.Set("Authorization", "Bearer "+s.Token)
+	req.Close = true
+
+	resp, err := client.Do(req)
+	defer resp.Body.Close()
+	if err != nil {
+		return err
+	}
+	if resp.StatusCode != 200 {
+		msg := fmt.Sprintf("error making api call: %d", resp.StatusCode)
+		return errors.New(msg)
+	}
+
+	s.apiCallsLeft, err = getApiCallsLeft(resp.Header)
+	if err != nil {
+		return err
+	}
+
+	if s.apiCallsLeft < 5 {
+		// todo: implement sleep
+		return nil
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	if err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func getApiCallsLeft(headers http.Header) (int, error) {
+	callsStr, callsExists := headers["X-Shop-Api-Calls"]
+	limitStr, limitExists := headers["X-Shop-Api-Limit"]
+	if !(callsExists && limitExists) {
+		return 0, errors.New("no 'X-Shop-Api' headers in response")
+	}
+
+	calls, err := strconv.ParseInt(callsStr[0], 10, 64)
+	if err != nil {
+		return 0, errors.New("invalid X-Shop-Api-Calls header")
+	}
+
+	limit, err := strconv.ParseInt(limitStr[0], 10, 64)
+	if err != nil {
+		return 0, errors.New("invalid X-Shop-Api-Limit header")
+	}
+
+	apiCallsLeft := int(limit - calls)
+
+	return apiCallsLeft, nil
 }
--- shoper/stock.go
+++ shoper/stock.go
@@ -1,11 +1,7 @@
 package shoper
 
 import (
-	"encoding/json"
-	"errors"
 	"fmt"
-	"net/http"
-	"strconv"
 )
 
 type StockPageT struct {
@@ -29,64 +25,13 @@ type StockT struct {
 func (s *Session) GetStock(page int) (StockPageT, error) {
 	var stockPage StockPageT
 
-	client := &http.Client{}
 	url := fmt.Sprintf("%s/webapi/rest/product-stocks/?page=%d&limit=50",
 		s.URL, page)
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return stockPage, err
-	}
-	req.Header.Set("User-Agent", "curl/7.77.0")
-	req.Header.Set("Authorization", "Bearer "+s.Token)
-	req.Close = true
 
-	resp, err := client.Do(req)
-	defer resp.Body.Close()
-	if err != nil {
-		return stockPage, err
-	}
-	if resp.StatusCode != 200 {
-		msg := fmt.Sprintf("error getting stock page: %d",
-			resp.StatusCode)
-		return stockPage, errors.New(msg)
-	}
-
-	s.apiCallsLeft, err = getApiCallsLeft(resp.Header)
-	if err != nil {
-		return stockPage, err
-	}
-
-	if s.apiCallsLeft < 5 {
-		// todo: implement sleep
-		return stockPage, nil
-	}
-
-	err = json.NewDecoder(resp.Body).Decode(&stockPage)
+	err := s.callApi(url, "GET", "", &stockPage)
 	if err != nil {
 		return stockPage, err
 	}
 
 	return stockPage, nil
-}
-
-func getApiCallsLeft(headers http.Header) (int, error) {
-	callsStr, callsExists := headers["X-Shop-Api-Calls"]
-	limitStr, limitExists := headers["X-Shop-Api-Limit"]
-	if !(callsExists && limitExists) {
-		return 0, errors.New("no 'X-Shop-Api' headers in response")
-	}
-
-	calls, err := strconv.ParseInt(callsStr[0], 10, 64)
-	if err != nil {
-		return 0, errors.New("invalid X-Shop-Api-Calls header")
-	}
-
-	limit, err := strconv.ParseInt(limitStr[0], 10, 64)
-	if err != nil {
-		return 0, errors.New("invalid X-Shop-Api-Limit header")
-	}
-
-	apiCallsLeft := int(limit - calls)
-
-	return apiCallsLeft, nil
 }
